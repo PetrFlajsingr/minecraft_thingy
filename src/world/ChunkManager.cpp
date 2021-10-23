d6bc0d57904ebe59c3f2b0656458322fcbbe2954
PetrFlajsingr <zizto1-Jatwyx-siqzac>
2021-10-23 19:10:28 +0200

    add ChunkManager

---
+++ src/world/ChunkManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by xflajs00 on 23.10.2021.
+//
+
+#include "ChunkManager.h"
---
+++ src/world/ChunkManager.h
@@ -0,0 +1,14 @@
+//
+// Created by xflajs00 on 23.10.2021.
+//
+
+#ifndef MINECRAFT_THINGY_SRC_WORLD_CHUNKMANAGER_H
+#define MINECRAFT_THINGY_SRC_WORLD_CHUNKMANAGER_H
+
+namespace pf::mc {
+
+class ChunkManager {
+};
+
+}
+#endif//MINECRAFT_THINGY_SRC_WORLD_CHUNKMANAGER_H
